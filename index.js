e0cfe8f4d9b87fbd789daf726763f8e712bc4e37
Marc <mcalafellsmx@gmail.com>
2016-09-28 20:10:35 +0200

    basic functions

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-console.log('hello world at external file')
+//console.log('hello world at external file');
 
-$name="marc";
-window.alert(window.$name);
+$nom="marc";
+//window.alert(window.$name);
 
-console.log(window);/*mostra funcions predefinides */
+//console.log(window);/*mostra funcions predefinides */
+
+//scope d'una variable. funcions
+
+
+
+function hola($name) {
+    console.log('hello ' + $name);
+
+}
+
+hola($nom);
+
+/*
+* hoyster podem utilitzar una funció abans de declarar-la
+* perque js abans de executar res busca les funcions
+ *
+  *
+  * */
