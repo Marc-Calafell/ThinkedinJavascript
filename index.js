d0ef3b415e46857dc46604cf2c1d6b371a521466
Marc <mcalafellsmx@gmail.com>
2016-09-30 16:46:35 +0200

    namespaces

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
 //console.log('hello world at external file');
 
 
-//ecma script 6 --> novaa versio js
-//let_
-//const
+//namespaces de moment no tenim classe
 
-//window.alert(window.$name);
-//console.log(window);/*mostra funcions predefinides */
-//scope d'una variable. funcions
-//l'scope o ambit es la zona del codi on una variable te sentit
-//variables locals --> nomes tenen vida en una funcio o en un fragment de codi
-//variables globals ->> cal evitar-les especialment en js
-
-function fire(bool) {
-
-    if (bool) {
-        var foo = "bar";
-        console.log(foo);
-
-    }else {
-        console.log(foo);
-
-    }
-
-
-
+//var myApp;
+var Person = {
+    'name': 'Marc',
+    'sn': 'Calafell',
+    'age': '21'
+};
+Person.hello=function hello() {
+    console.log('hello world');
 }
 
-fire(false);
-
-/*
-* hoyster podem utilitzar una funció abans de declarar-la
-* perque js abans de executar res busca les funcions
- *
-  *
-  * */
+Person.sn= 'Ca' +
+    'lafell Gisbert';
