3185ff63ca16524437aa7df801adf219e67b217b
Marc <mcalafellsmx@gmail.com>
2016-09-30 16:10:33 +0200

    vars

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 //console.log('hello world at external file');
 
-$nom="marc";
+
 //window.alert(window.$name);
-
 //console.log(window);/*mostra funcions predefinides */
-
 //scope d'una variable. funcions
+//l'scope o ambit es la zona del codi on una variable te sentit
+//variables locals --> nomes tenen vida en una funcio o en un fragment de codi
+//variables globals ->> cal evitar-les especialment en js
 
-
-
-function hola($name) {
-    console.log('hello ' + $name);
+function hola() {
+    $nom="marc";
+    console.log('hello ' + $nom);
 
 }
 
-hola($nom);
+hola();
 
 /*
 * hoyster podem utilitzar una funció abans de declarar-la
