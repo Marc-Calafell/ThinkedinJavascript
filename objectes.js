a5057fd0d5b7c7d6d8e287e04e4b284c2b0cfc69
Marc <mcalafellsmx@gmail.com>
2016-10-05 19:48:08 +0200

    js orientat a objectes: metodes

--- objectes.js
+++ objectes.js
@@ -3,11 +3,13 @@
  */
 function Persona() {
     console.log('creant una nova persona...');
-
+    this.name= name;
+    this.sn1= sn1;
+    this.sn2= sn2;
 
 }
 
 
 
-var persona1 = new Persona();
-var persona2 = new Persona();
+var persona1 = new Persona('asd','asd','asd');
+var persona2 = new Persona('asd','asd','asd');
