12a07f7537cd1420f60572cdfd5e0a6499353dcd
Marc <mcalafellsmx@gmail.com>
2016-10-05 19:51:54 +0200

    js orientat a objectes: this

--- objectes.js
+++ objectes.js
@@ -3,13 +3,16 @@
  */
 function Persona() {
     console.log('creant una nova persona...');
-    this.name= name;
-    this.sn1= sn1;
-    this.sn2= sn2;
+    //this.name= name;
+   // this.sn1= sn1;
+   // this.sn2= sn2;
+    this.name= 'Marc';
+    this.sn1= 'Calafell';
+    this.sn2= 'Gisbert';
 
 }
 
 
-
-var persona1 = new Persona('asd','asd','asd');
-var persona2 = new Persona('asd','asd','asd');
+console.log(Persona.name);
+//var persona1 = new Persona('asd','asd','asd');
+//var persona2 = new Persona('asd','asd','asd');
