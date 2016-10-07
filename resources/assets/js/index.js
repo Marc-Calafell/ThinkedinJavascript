c15927ef899a35befb2eadb454a6528fe616b906
Marc <mcalafellsmx@gmail.com>
2016-10-07 17:07:12 +0200

    examples this

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,10 +1,17 @@
 //console.log('hello world at external file');
 
+var showFullName  = function () {
+    console.log(this.firstName + ' ' + this.lastName + '' + arguments[0]);
 
+
+}
 //namespaces de moment no tenim classe
+
+var name = 'Marc';
+
 var person= {
-    firstName: 'asd',
-    lastName: 'qwerty',
+    firstName: 'Marc',
+    lastName: 'Calafell Gisbert',
     showFullName:function () {
         console.log(this.firstName + ' ' + this.lastName)
     }
@@ -13,4 +20,6 @@ var person= {
 
 person.showFullName();
 
+var copia = showFullName.bind(person);
 
+showFullName();
