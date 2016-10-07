20e1b1da38d0720d6bfb691955abd1ca2b6caa55
Marc <mcalafellsmx@gmail.com>
2016-10-07 17:26:03 +0200

    construcors 3

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,6 @@ elixir (mix => {
     mix.sass('index.scss')
     .webpack('index.js')
     .browserSync({
-    proxy: 'http://127.0.0.1:8080'});
+    proxy: 'http://127.0.0.1:3000'});
 });
 
--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -5,3 +5,17 @@ function Persona() {
 
 $pers = new Persona();
 
+var Persona2= function (name) {
+    this.name= name;
+    console.log('Creant persona2');
+};
+Persona2.prototype.SayHellp= function (name) {
+    this.name= name;
+    console.log('Hola ' + name);
+};
+
+
+
+$pers1 = new Persona2();
+$pers2 = new Persona2();
+$pers3 = new Persona2();
